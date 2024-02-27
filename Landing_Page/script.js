2126e004a4461fb6ea3f64ab79976b987174c87d
Muhammad Sair <msair565@gmail.com>
2024-02-27 08:57:26 +0500

    jspractice

--- Landing_Page/script.js
+++ Landing_Page/script.js
@@ -51,3 +51,48 @@ function outside(n) {
 const fnouside=outside(7)
 console.log(fnouside(5))
 console.log(outside(4)(7)) 
+let age =(a,b)=> a+b;
+console.log(age(8,9))
+
+
+const person = {fname:"John", lname:"Doe", age:25}; 
+let txt = "";
+for (let x in person) {
+  txt += person[x] + " ";
+}
+console.log(txt)
+
+
+const numbers=[1,5,32,78,4,2,3]
+// const comapringFunction=function compareNumbers(a, b) {
+//     return a - b;
+//   }
+numbers.sort((a,b)=> a-b)
+console.log(numbers)  
+const items = [
+    { name: "Edward", value: 21 },
+    { name: "Sharpe", value: 37 },
+    { name: "And", value: 45 },
+    { name: "The", value: -12 },
+    { name: "Magnetic", value: 13 },
+    { name: "Zeros", value: 37 },
+  ];
+  
+  // sort by value
+  items.sort((a, b) => a.value - b.value);
+  
+  // sort by name
+  items.sort((a, b) => {
+    const nameA = a.name.toUpperCase() // ignore upper and lowercase
+    const nameB = b.name.toUpperCase() // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
+  });
+console.log(items)  
