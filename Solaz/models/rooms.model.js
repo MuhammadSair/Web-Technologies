116fd7d214a1e4da543a53489e4f8840b0660d69
Muhammad Sair <msair565@gmail.com>
2024-05-22 21:21:35 +0500

    home-page-frontend

---
+++ Solaz/models/rooms.model.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const roomsSchema = mongoose.Schema({
+  room: SingleBed,
+});
--- Solaz/server.js
+++ Solaz/server.js
@@ -1,2 +1,18 @@
-let express=require ("express")
-let server=express()
+let express = require("express");
+let mongoose = require("mongoose");
+let server = express();
+
+const roomSchema = new mongoose.Schema({
+  name: String,
+  roomnumber: Number,
+});
+
+server.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
+server.get("/", (req, res) => {
+  res.render("index");
+});
+mongoose.connect("mongodb://localhost:27017/").then(() => {
+  console.log("Connected to MongoDB");
+});
